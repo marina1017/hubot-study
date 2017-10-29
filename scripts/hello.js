cadb3c9b08e5a2cb1ac22f21ddc03036533ad94b
Marina Nakagawa <mari10172001@yahoo.co.jp>
2017-10-29 14:07:39 +0000

    中身の修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,10 @@ module.exports = (robot) => {
         const username = msg.message.user.name;
         msg.send('Hello, ' + username);
     });
+    robot.hear(/pai>/i, (msg) => {
+	const username = msg.message.user.name;
+        const lots = ['(  ・ 　λ　 ・   )Hカップ', '(・　人　・)Gカップ', '(・ 人 ・)Fカップ', '(・人・)Eカップ', '(・   ・)Dカップ', '(・ ・)Cカップ', '(     )Bカップ', '|     |Aカップ'];
+        const lot = lots[Math.floor(Math.random() * lots.length)];
+        msg.send(username + 'は' + lot + 'です');	
+    });
 };
